0d26d2ddcf7a61f1d7eedaa783b1d35169a80a4c
antklim <antklim@gmail.com>
2021-11-13 15:36:11 +1100

    API GW role and policy added

--- .aws/main.sh
+++ .aws/main.sh
@@ -16,29 +16,62 @@ fi
 stack_name=$PROJECT
 stack_output_file=$stack_name-output.txt
 
-echo "Creating resources for $PROJECT project ..."
-aws cloudformation create-stack --stack-name $stack_name \
-  --template-body file://main.yml \
-  --parameters ParameterKey=ProjectName,ParameterValue=$PROJECT \
-  ParameterKey=TableName,ParameterValue=$TABLE \
-  --tags Key=project,Value=$PROJECT \
-  --region $REGION \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --output text > stack_output_file 
+stacks=$(aws cloudformation describe-stacks \
+  --query "Stacks[?StackName=='$stack_name']" \
+  --output json | jq '. | length')
 
-status=$?
-if [ $status -ne 0 ] ; then
-  echo "error: failed to initiate stack $stack_name"
-  exit 1
+if [ $stacks -eq 0 ] ; then
+  echo "Creating resources for $PROJECT project ..."
+  aws cloudformation create-stack --stack-name $stack_name \
+    --template-body file://main.yml \
+    --parameters ParameterKey=ProjectName,ParameterValue=$PROJECT \
+    ParameterKey=TableName,ParameterValue=$TABLE \
+    --tags Key=project,Value=$PROJECT \
+    --region $REGION \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --output text > $stack_output_file
+
+  status=$?
+  if [ $status -ne 0 ] ; then
+    echo "error: failed to initiate stack $stack_name"
+    exit 1
+  fi
+
+  echo "Waiting for resources creation completion ..."
+  aws cloudformation wait stack-create-complete --stack-name $stack_name
+
+  status=$?
+  if [ $status -ne 0 ] ; then
+    echo "error: failed to create stack $stack_name"
+    exit 1
+  fi
+
+  echo "$PROJECT resources successfully created."
+else
+  echo "Updating resources for $PROJECT project ..."
+
+  aws cloudformation update-stack --stack-name $stack_name \
+    --template-body file://main.yml \
+    --parameters ParameterKey=ProjectName,UsePreviousValue=true \
+    ParameterKey=TableName,UsePreviousValue=true \
+    --region $REGION \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --output text > $stack_output_file
+
+  status=$?
+  if [ $status -ne 0 ] ; then
+    echo "error: failed to initiate stack update $stack_name"
+    exit 1
+  fi
+
+  echo "Waiting for resources update completion ..."
+  aws cloudformation wait stack-update-complete --stack-name $stack_name
+
+  status=$?
+  if [ $status -ne 0 ] ; then
+    echo "error: failed to update stack $stack_name"
+    exit 1
+  fi
+
+  echo "$PROJECT resources successfully updated."
 fi
-
-echo "Waiting for resources creation completion ..."
-aws cloudformation wait stack-create-complete --stack-name $stack_name
-
-status=$?
-if [ $status -ne 0 ] ; then
-  echo "error: failed to create stack $stack_name"
-  exit 1
-fi
-
-echo "$PROJECT resources successfully created."
