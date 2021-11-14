5b6a4c4652fa8fc6b6005732d4dbebac29a944b8
antklim <antklim@gmail.com>
2021-11-15 07:07:30 +1100

    Form submit WIP

---
+++ form/js/main.js
@@ -0,0 +1,19 @@
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('submitIntentionBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
+function submitIntention() {
+  console.log('Submiting intention...')
+}
