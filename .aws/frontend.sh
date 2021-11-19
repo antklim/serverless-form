1800f0b7b588f00500f6b8125f71c73acd79c455
antklim <antklim@gmail.com>
2021-11-19 16:22:45 +1100

    Frontend upload script added

---
+++ .aws/frontend.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+################################################################################
+##
+## The following script synchronises frontend resources with dedicated S3 bucket
+##
+################################################################################
+
+. .env
+
+REGION=${REGION:-"ap-southeast-2"}
+
+if [ -z $FRONTEND_BUCKET_NAME ] ; then
+  echo "error: FRONTEND_BUCKET_NAME required"
+  exit 1
+fi
+
+cd ../form
+rm -rf dist
+npm run build
+
+aws s3 sync dist/ s3://$FRONTEND_BUCKET_NAME/ \
+  --cache-control "max-age=300, must-revalidate" \
+  --delete
